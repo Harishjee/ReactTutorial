075c8bcefdbbf9d390b07c0d10d85d8bf0612092
Harish <pixelsmedialab@gmail.com>
2023-04-19 13:08:02 +0530

    ep-01-inception

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ReactTutorial
+
+
+# Parcel - Parcel is such a Beast
+
+- Dev Build
+- Local Server
+- HMR >Hot Module Replacement - chnaging the each changes , instantly its reflecting in browser without refreshing
+- file watcher algoritham - written in C++ > keep tracking all the file and once the changes made , its automatically updating in browser 
+- Caching - Faster Development builds > its caching the file which is already in build, and its only build and replace the which file made change. so it is making developmnet build fast.
+- Image Optimization - its basically do image optimization
+- Minification files - its used to do the mifiy the all the file in production
+- Bundling - its used to do the budle all the files
+- Compression - compress alll the files
+- Consistent Hashing
+- File Splitting
+- Diffrential Bundling - it used to support the older browsers (diffrent type of older browsers) 
+- Diagnostics
+- Error Handling - gives you to easily understandable errors in cosole
+- HTTPS
+- Tree Shaking - remove unused code
--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// const heading = React.createElement("h1",
-// {id: "heading", className: "abc" }, 
-// "Hiii");
-// console.log(heading);
-//     const root = ReactDOM.createRoot(document.getElementById("root"));
-//     root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 
 const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child", key: "1" }, [React.createElement("h1", {}, "I am the H1"), React.createElement("h2", {}, "I am the H2")]), React.createElement("div", { id: "child2", key: "2" }, [React.createElement("h1", {}, "I am the H1"), React.createElement("h2", {}, "I am the H2")])]);
+    [React.createElement("div", { id: "child", key: "1" }, [React.createElement("h1", {}, "shdushishi busubsud nfnfd"), React.createElement("h2", {}, "I am the H2")]), React.createElement("div", { id: "child2", key: "2" }, [React.createElement("h1", {}, "I am the H1"), React.createElement("h2", {}, "I am the H2")])]);
 
-console.log(parent);
+// console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
